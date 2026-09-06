e18e0a24165c8810fbe957d73132f5d63a10326c
infinitie <infinitiecity@gmail.com>
2026-09-07 00:37:13 +0300

    memory management of the dynamic array that monitors the code

--- memory.h
+++ memory.h
@@ -5,4 +5,14 @@
 #ifndef PFORGE_MEMORY_H
 #define PFORGE_MEMORY_H
 
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), \
+        sizeof(type) * (newCount))
+
+
+void* reallocate(void *pointer, size_t oldSize, size_t newSize);
 #endif //PFORGE_MEMORY_H
